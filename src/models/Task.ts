1fe3297b524adf517c96859223ce60d3c0ba3e4c
Jhordy Abonia <jhordy.abonia@gmail.com>
2025-08-06 12:29:10 -0500

    init:added data model

---
+++ src/models/Task.ts
@@ -0,0 +1,39 @@
+'use-strict'
+
+class  Task {
+    _id:number
+    title:string
+    status:string
+    priority:number
+    description:string
+    created_date:Date        
+    due_date:Date
+    assigned_to:string
+    author:string
+    project_id:number
+    tags = []
+    permissions = []
+    
+    constructor() {}
+
+    static STATUS = ["pending","prosessing","completed"]
+    
+    static parser = (data:object):Task=>{
+        const task = new Task()
+        task._id            = data._id?data._id:task._id
+        task.title          = data.title?data.title:task.title
+        task.priority       = data.priority?data.priority:task.priority
+        task.status         = data.status?data.status:task.status
+        task.description    = data.description?data.description:task.description
+        task.created_date   = data.created_date?data.created_date:task.created_date
+        task.due_date       = data.due_date?data.due_date:task.due_date
+        task.assigned_to    = data.assigned_to?data.assigned_to:task.assigned_to
+        task.author         = data.author?data.author:task.author
+        task.project_id     = data.project_id?data.project_id:task.project_id
+        task.tags           = data.tags?data.tags:task.tags
+        task.permissions    = data.permissions?data.permissions:task.permissions
+        return task
+    }
+}
+
+export { Task }
